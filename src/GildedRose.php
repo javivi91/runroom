4f029a0ce2b02e645d70c216de39a124e41c8582
Javier Alcantara <alcantara.javi@gmail.com>
2018-06-13 20:47:52 +0200

    (REFACTOR) Create increaseQuality, reduceQuality, reduceSellIn methods in Item

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -2,59 +2,51 @@
 
 namespace Runroom\GildedRose;
 
-class GildedRose {
-
+class GildedRose
+{
     private $items;
 
-    function __construct($items) {
+    function __construct($items)
+    {
         $this->items = $items;
     }
 
-    function update_quality() {
+    function update_quality()
+    {
         foreach ($this->items as $item) {
             if ($item->getName() != 'Aged Brie' and $item->getName() != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->getQuality() > 0) {
-                    if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
-                        $item->setQuality($item->getQuality() - 1);
-                    }
+                if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
+                    $item->reduceQuality();
                 }
             } else {
                 if ($item->getQuality() < 50) {
                     $item->setQuality($item->getQuality() + 1);
                     if ($item->getName() == 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->getSellIn() < 11) {
-                            if ($item->getQuality() < 50) {
-                                $item->setQuality($item->getQuality() + 1);
-                            }
+                            $item->increaseQuality();
                         }
                         if ($item->getSellIn() < 6) {
-                            if ($item->getQuality() < 50) {
-                                $item->setQuality($item->getQuality() + 1);
-                            }
+                            $item->increaseQuality();
                         }
                     }
                 }
             }
 
             if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
-                $item->setSellIn($item->getSellIn() - 1);
+                $item->reduceSellIn();
             }
 
             if ($item->getSellIn() < 0) {
                 if ($item->getName() != 'Aged Brie') {
                     if ($item->getName() != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->getQuality() > 0) {
-                            if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
-                                $item->setQuality($item->getQuality() - 1);
-                            }
+                        if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
+                            $item->reduceQuality();
                         }
                     } else {
                         $item->setQuality(0);
                     }
                 } else {
-                    if ($item->getQuality() < 50) {
-                        $item->setQuality($item->getQuality() + 1);
-                    }
+                    $item->increaseQuality();
                 }
             }
         }
--- src/Item.php
+++ src/Item.php
@@ -4,12 +4,14 @@ namespace Runroom\GildedRose;
 
 class Item
 {
+    const MIN_QUALITY = 0;
+    const MAX_QUALITY = 50;
 
     private $name;
     private $sellIn;
     private $quality;
 
-    function __construct($name, $sellIn, $quality)
+    public function __construct($name, $sellIn, $quality)
     {
         $this->name = $name;
         $this->sellIn = $sellIn;
@@ -19,6 +21,33 @@ class Item
     public function __toString()
     {
         return "{$this->name}, {$this->sellIn}, {$this->quality}";
+    }
+
+    /**
+     * Increase quality if not exceeds the limit
+     */
+    public function increaseQuality($sum = 1)
+    {
+        if ($this->quality < self::MAX_QUALITY) {
+            $this->setQuality($this->quality + $sum);
+        }
+    }
+
+    /**
+     * Reduce quality if not exceeds the limit
+     */
+    public function reduceQuality($num = 1) {
+        if ($this->quality > self::MIN_QUALITY) {
+            $this->setQuality($this->quality - $num);
+        }
+    }
+
+    /**
+     * Reduce sell in
+     */
+    public function reduceSellIn($num = 1)
+    {
+        $this->setSellIn($this->sellIn - $num);
     }
 
     /**
