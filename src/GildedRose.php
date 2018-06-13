2892a69be992b35f69150bc4e9aa0f8fe64c4fc5
Javier Alcantara <alcantara.javi@gmail.com>
2018-06-13 21:33:23 +0200

    (REFACTOR) updateQuality method

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,9 +4,9 @@ namespace Runroom\GildedRose;
 
 class GildedRose
 {
-    const LIMIT_LEVEL_1 = 6;
-    const LIMIT_LEVEL_2 = 11;
-    const MAX_SELL_IN = 0;
+    const SELLIN_LIMIT_0 = 0;
+    const SELLIN_LIMIT_1 = 5;
+    const SELLIN_LIMIT_2 = 10;
 
     private $items;
 
@@ -15,44 +15,72 @@ class GildedRose
         $this->items = $items;
     }
 
+    /**
+     * Update quality of items
+     */
     public function updateQuality()
     {
         foreach ($this->items as $item) {
-            if ($item->getName() != 'Aged Brie' && $item->getName() != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
-                    $item->reduceQuality();
-                }
-            } else {
-                if ($item->getQuality() < 50) {
-                    $item->setQuality($item->getQuality() + 1);
-                    if ($item->getName() == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->getSellIn() < self::LIMIT_LEVEL_2) {
-                            $item->increaseQuality();
-                        }
-                        if ($item->getSellIn() < self::LIMIT_LEVEL_1) {
-                            $item->increaseQuality();
-                        }
-                    }
-                }
+            switch ($item->getName()) {
+                case 'Sulfuras, Hand of Ragnaros':
+                    break;
+                case 'Aged Brie':
+                    $this->updateAgedQuality($item);
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    $this->updateBackstageQuality($item);
+                    break;
+                default:
+                    $this->updateDefaultQuality($item);
+                    break;
             }
+        }
+    }
 
-            if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
-                $item->reduceSellIn();
-            }
+    /**
+     * Update quality of 'Aged Brie' item
+     */
+    private function updateAgedQuality($item)
+    {
+        $item->reduceSellIn();
 
-            if ($item->getSellIn() < self::MAX_SELL_IN) {
-                if ($item->getName() != 'Aged Brie') {
-                    if ($item->getName() != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
-                            $item->reduceQuality();
-                        }
-                    } else {
-                        $item->setQuality(0);
-                    }
-                } else {
-                    $item->increaseQuality();
-                }
-            }
+        if ($item->getSellIn() < self::SELLIN_LIMIT_0) {
+            $item->increaseQuality(2);
+        } else {
+            $item->increaseQuality();
+        }
+    }
+
+    /**
+     * Update quality of 'Backstage TAFKAL80ETC' item
+     */
+    private function updateBackstageQuality($item)
+    {
+        $item->reduceSellIn();
+
+        if ($item->getSellIn() < self::SELLIN_LIMIT_0) {
+            $item->setQuality(0);
+        } elseif ($item->getSellIn() < self::SELLIN_LIMIT_1) {
+            $item->increaseQuality(3);
+        } elseif ($item->getSellIn() < self::SELLIN_LIMIT_2) {
+            $item->increaseQuality(2);
+        } else {
+            $item->increaseQuality();
+        }
+
+    }
+
+    /**
+     * Update quality of default item
+     */
+    private function updateDefaultQuality($item)
+    {
+        $item->reduceSellIn();
+
+        if ($item->getSellIn() < self::SELLIN_LIMIT_0) {
+            $item->reduceQuality(2);
+        } else {
+            $item->reduceQuality();
         }
     }
 }
