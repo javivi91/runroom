4d5f5d56b3677d9a8783f048d7be0f5f9e528e11
Javier Alcantara <alcantara.javi@gmail.com>
2018-06-13 20:17:09 +0200

    (REFACTOR) Create Item getters and setters

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -12,48 +12,48 @@ class GildedRose {
 
     function update_quality() {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+            if ($item->getName() != 'Aged Brie' and $item->getName() != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->getQuality() > 0) {
+                    if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
+                        $item->setQuality($item->getQuality() - 1);
                     }
                 }
             } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                if ($item->getQuality() < 50) {
+                    $item->setQuality($item->getQuality() + 1);
+                    if ($item->getName() == 'Backstage passes to a TAFKAL80ETC concert') {
+                        if ($item->getSellIn() < 11) {
+                            if ($item->getQuality() < 50) {
+                                $item->setQuality($item->getQuality() + 1);
                             }
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                        if ($item->getSellIn() < 6) {
+                            if ($item->getQuality() < 50) {
+                                $item->setQuality($item->getQuality() + 1);
                             }
                         }
                     }
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
+            if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
+                $item->setSellIn($item->getSellIn() - 1);
             }
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
+            if ($item->getSellIn() < 0) {
+                if ($item->getName() != 'Aged Brie') {
+                    if ($item->getName() != 'Backstage passes to a TAFKAL80ETC concert') {
+                        if ($item->getQuality() > 0) {
+                            if ($item->getName() != 'Sulfuras, Hand of Ragnaros') {
+                                $item->setQuality($item->getQuality() - 1);
                             }
                         }
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->setQuality(0);
                     }
                 } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                    if ($item->getQuality() < 50) {
+                        $item->setQuality($item->getQuality() + 1);
                     }
                 }
             }
--- src/Item.php
+++ src/Item.php
@@ -2,20 +2,73 @@
 
 namespace Runroom\GildedRose;
 
-class Item {
+class Item
+{
 
-    public $name;
-    public $sell_in;
-    public $quality;
+    private $name;
+    private $sellIn;
+    private $quality;
 
-    function __construct($name, $sell_in, $quality) {
+    function __construct($name, $sellIn, $quality)
+    {
         $this->name = $name;
-        $this->sell_in = $sell_in;
+        $this->sellIn = $sellIn;
         $this->quality = $quality;
     }
 
-    public function __toString() {
-        return "{$this->name}, {$this->sell_in}, {$this->quality}";
+    public function __toString()
+    {
+        return "{$this->name}, {$this->sellIn}, {$this->quality}";
     }
 
+    /**
+     * Get the value of Name
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of Name
+     */
+    public function setName(string $name) : Item
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get the value of Sell In
+     */
+    public function getSellIn(): int
+    {
+        return $this->sellIn;
+    }
+
+    /**
+     * Set the value of Sell In
+     */
+    public function setSellIn(int $sellIn) : Item
+    {
+        $this->sellIn = $sellIn;
+        return $this;
+    }
+
+    /**
+     * Get the value of Quality
+     */
+    public function getQuality(): int
+    {
+        return $this->quality;
+    }
+
+    /**
+     * Set the value of Quality
+     */
+    public function setQuality(int $quality): Item
+    {
+        $this->quality = $quality;
+        return $this;
+    }
 }
